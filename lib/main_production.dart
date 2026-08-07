015662584c8216665ebafb3f6776f2caf1d5990e
Omar Abd El Raouf <omarraouf1899@gmail.com>
2026-08-07 14:30:35 +0300

    -added fastlane to android and connected it to firebase app distribution

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,10 +1,13 @@
 import 'package:docdoc/core/di/dependency_injection.dart';
 import 'package:docdoc/doc_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil_plus/flutter_screenutil_plus.dart';
 
 import 'core/routing/app_router.dart';
 
-void main() {
-  init();
+void main() async {
+  await init();
+  await ScreenUtilPlus.ensureScreenSize();
+
   runApp(DocApp(appRouter: AppRouter()));
 }
